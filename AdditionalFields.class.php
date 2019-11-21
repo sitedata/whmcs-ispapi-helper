1ffba4643a30ce4f754be8c6a921ade4c7772a43
Kai Schwarz <kschwarz@hexonet.net>
2019-11-21 16:22:47 +0100

    style(psr2): set formatting

--- AdditionalFields.class.php
+++ AdditionalFields.class.php
@@ -8,17 +8,18 @@ class AdditionalFields extends \WHMCS\Domains\AdditionalFields
      * @param array $command API command to add additional domain field parameters to
      * @param string $registrantcountry country of the registrant
      */
-    public function addToCommand(&$command, $registrantcountry) {
-        foreach($this->getFields() as $fieldKey => $values){
+    public function addToCommand(&$command, $registrantcountry)
+    {
+        foreach ($this->getFields() as $fieldKey => $values) {
             $iname = $this->getConfigValue($fieldKey, "Ispapi-Name");
-            if (empty($iname)){
+            if (empty($iname)) {
                 continue;
             }
             $ignoreCountries = $this->getConfigValue($fieldKey, "Ispapi-IgnoreForCountries");
             if (!(
-                empty($ignoreCountries) || 
+                empty($ignoreCountries) ||
                 (!empty($registrantcountry) && !in_array(strtoupper($registrantcountry), $ignoreCountries))
-            )){
+            )) {
                 continue;
             }
             $type = $this->getConfigValue($fieldKey, "Type");
@@ -33,32 +34,31 @@ class AdditionalFields extends \WHMCS\Domains\AdditionalFields
             if (!empty($format) && $format == "UPPERCASE") {
                 $val = strtoupper($val);
             }
-            if (!empty($val)){
+            if (!empty($val)) {
                 $command[$iname] = $val;
             }
         }
     }
 
     //untested! TODO
-    public function setFieldValuesFromAPI($r) {
-        if ($r["CODE"] == "200"){
+    public function setFieldValuesFromAPI($r)
+    {
+        if ($r["CODE"] == "200") {
             $data = [];
             $r = $r["PROPERTY"];
-            foreach($this->getFields() as $fieldKey => $values){
+            foreach ($this->getFields() as $fieldKey => $values) {
                 $type = $this->getConfigValue($fieldKey, "Type");
                 $iname = $this->getConfigValue($fieldKey, "Ispapi-Name");
                 $name = $this->getConfigValue($fieldKey, "Name");
                 $defaultval = $this->getConfigValue($fieldKey, "Default");
-                if (isset($r[$iname][0])){
+                if (isset($r[$iname][0])) {
                     $data[$name] = $r[$iname][0];
-                    if ($type == "tickbox" && $data[$name] == "1"){
+                    if ($type == "tickbox" && $data[$name] == "1") {
                         $data[$name] = $defaultval;
                     }
                 }
             }
-            parent::setFieldValues($data);    
-        } 
+            parent::setFieldValues($data);
+        }
     }
 }
-
-?>
